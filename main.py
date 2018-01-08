2dc74777e96810553671c7a9b466cbe995a53fa0
Roland Arnoldt <roland@ouiouioui.space>
2018-01-08 17:56:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,18 @@ i=0
 rand_word = []
 new_word = ""
 counter = 0
-probability = 144**27
 success_count = 0
 sentence = "quels que soient les progres des connaissances humaines, il y aura toujours place pour l'ignorance et par suite pour le hasard et la probabilite"
 # "whatever the progress of human knowledge, there will always be room for ignorance, hence for chance and probability"
 # Emile Borel (1914). Le hasard. Librairie Felix Alcan. p. 12-13
 
 letters = ''.join(string.ascii_lowercase)
+print(" ")
 print("Predicting with a random engine based on quantum processes:")
 print('"%s"' %(sentence))
 print(" ")
 print('(whatever the progress of human knowledge, there will always be room for ignorance, hence for chance and probability)')
 print ("Emile Borel (1914). Le hasard. Librairie Felix Alcan. p. 12-13")
-print(" ")
-print("probability for the whole sentence is 1:%i" %(probability))
 print(" ")
 
 # generate quote randomly using quantum processes
